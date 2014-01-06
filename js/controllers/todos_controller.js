58ece9322b6fcca1be7879cdfedc7d3a902a54bc
Sohaib Bhatti <sohaibbbhatti@gmail.com>
2014-01-06 20:01:33 +0500

    create new todo

---
+++ js/controllers/todos_controller.js
@@ -0,0 +1,16 @@
+Todos.TodosController = Ember.ArrayController.extend({
+  actions: {
+    createTodo: function () {
+      var title = this.get('newTitle');
+      if (!title.trim()) { return; }
+
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      this.set('newTitle', '');
+      todo.save();
+    }
+  }
+});
