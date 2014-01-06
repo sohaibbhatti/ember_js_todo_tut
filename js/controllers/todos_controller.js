2ffc2d2fea4748d5ba46e98079a4fc2f053972b1
Sohaib Bhatti <sohaibbbhatti@gmail.com>
2014-01-07 03:19:52 +0500

    clear completed functionality

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -11,6 +11,12 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       this.set('newTitle', '');
       todo.save();
+    },
+
+    clearCompleted: function () {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
 
@@ -21,6 +27,14 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function () {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  hasCompleted: function () {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function () {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted')
 
 });
