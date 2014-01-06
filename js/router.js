e85f98cf9121e38c7ca74b99668740a4b72a8905
Sohaib Bhatti <sohaibbbhatti@gmail.com>
2014-01-06 16:25:26 +0500

    iterate through model instead of static view

--- js/router.js
+++ js/router.js
@@ -2,3 +2,9 @@ Todos.Router.map(function() {
   // Render todos template on route '/'
   this.resource('todos', { path: '/' });
 });
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('todo');
+  }
+});
